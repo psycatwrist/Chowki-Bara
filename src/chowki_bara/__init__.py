aa25c03c6bc6867414adc0d37612647df2148e45
Psycatwrist <psycatwrist+github@gmail.com>
2026-08-21 14:57:19 +0530

    Update __all__ to include implemented components

--- src/chowki_bara/__init__.py
+++ src/chowki_bara/__init__.py
@@ -4,5 +4,5 @@ from chowki_bara.utils.cowrie_shells import CowrieShells
 from chowki_bara.components.board import Board
 from chowki_bara.components.player import Player
 
-# (2) All Importing
+# (2) Implementing '*'
 __all__ = ["CowrieShells", "Board", "Player"]
