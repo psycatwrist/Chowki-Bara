67ab25403df851fc3a803c5bc843f42cd057dacd
Psycatwrist <psycatwrist+github@gmail.com>
2026-08-22 15:12:36 +0530

    Improve player piece movement and assertions
    Refactor player piece handling and assertions for game logic.

--- src/chowki_bara/engine.py
+++ src/chowki_bara/engine.py
@@ -19,6 +19,7 @@ class Engine:
         # (2.1.1) Initializing required variables
         self.players = players
         
+        assert players, "At least one player must be provided."
         assert len({player.board for player in players}) <= 1, "Every provided player object should be imitialozed with the same board."
         self.board = list({player.board for player in players})[0]
         
@@ -31,7 +32,7 @@ class Engine:
         # (2.1.3) Locking all pieces initially
         for player in players:
             for piece in player.pieces:
-                setattr(piece, is_unlocked, False)
+                setattr(piece, "is_unlocked", False)
      
     # (2.2) A convenient API method to roll
     def roll(self):
@@ -42,31 +43,24 @@ class Engine:
         assert self.current_shell_value is not None, "Please roll the dice first."
         
         # (2.3.1) Moving the piece if it is unlocked, or unlock if conditions are met
-        if self.board.pending_player[piece_index].is_unlocked or not self.has_won(self.pending_player):
-            self.board.pending_player[piece_index].move_steps(self.board.current_shell_value)
+        if self.board.pending_player.pieces[piece_index].is_unlocked and not self.has_won(self.board.pending_player):
+            self.board.pending_player.pieces[piece_index].move_steps(self.board.current_shell_value)
         else:
             if self.board.current_shell_value in self.shells.opens_pieces_on:
-                self.board.pending_player[piece_index].is_unlocked = True
+                self.board.pending_player.pieces[piece_index].is_unlocked = True
         
-        # (2.3.2) Updating castled_pieces values        
-        if pending_player.pieces[piece_index].current_pos == (0, 0):
-            if not hasattr(pending_player, castled_pieces):
-                pending_player.castled_pieces = 1
-            else:
-                pending_player.castled_pieces += 1    
-                    
-        # (2.3.3) Post movement steps
+        # (2.3.2) Post movement steps
         self.board.last_player = self.board.pending_player
         self.board.last_player.index = piece_index
         
         if not self.board.current_shell_value in self.shells.rewarding_numbers:
-            self.board.pending_player = (self.board.pending_player + 1) % len(self.players) # Rotating Player if the numbers aren't rewarding
+            self.board.pending_player = self.players[((self.players.index(pending_player) + 1) % len(self.players))] # Rotating Player if the numbers aren't rewarding
             
         self.board.current_shell_value = None # Resetting shell value
         
-        return self.board.last_player[self.board.last_player.index].current_pos
+        return self.board.last_player.pieces[self.board.last_player.index].current_pos
     
     # (2.4) A method to investigate if a player has won
     def has_won(self, player):
-        if value := all(piece.current_pos == (0, 0) for piece in player.pieces):
-            return value
+        value = all(piece.current_pos == (0, 0) for piece in player.pieces):
+        return value
